dd3b7fe153f72710efd582962eab90cec9442845
aniket.satarkar <aniketsatarkar9@gmail.com>
2017-08-13 02:26:04 +0530

    script for nodejs static server is added.

---
+++ local-server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('dist'));
+
+var server = app.listen(8000, function() 
+{
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('AngularRouting app listning at http://%s:%s', host, port); 
+});
